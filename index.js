73452f11fed133f0f91f9699843c5eef54e53cbc
benjaminbtrance <benjaminb.tran@gmail.com>
2021-06-30 14:53:48 -0500

    require inquirer, fs, generateMarkDown.js. add the first question to the questions array. create a writeFile function. create a inquirer.prompt to ask questions then write the file to a README file.

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkDown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: 'input',
+		message: 'What is the name of your project?',
+		name: 'title',
+	},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err) =>
+		err ? console.log(err) : console.log('Successfully created README!')
+	);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+	inquirer.prompt(questions).then((data) => {
+		writeToFile('README.md', generateMarkDown(data));
+	});
+}
 
 // Function call to initialize app
 init();
